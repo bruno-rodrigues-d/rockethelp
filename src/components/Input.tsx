fb499d9708a7590c97d034ef0c93fea20b30b991
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2022-07-20 14:44:00 -0300

    Buton

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import { Input as NativeBaseInput, IInputProps} from "native-base";
+import { Input as NativeBaseInput, IInputProps } from "native-base";
 
 export function Input({ ...rest }: IInputProps) {
   return (
@@ -11,7 +11,12 @@ export function Input({ ...rest }: IInputProps) {
       fontFamily="body"
       color="white"
       placeholderTextColor="gray.300"
-      { ...rest }
+      _focus={{
+        borderWidth: 1,
+        borderColor: "green.500",
+        bg: "gray.700",
+      }}
+      {...rest}
     />
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,13 @@
 import { VStack, Heading, Icon, useTheme } from "native-base";
 import Logo from "../assets/logo_primary.svg";
-import { Input } from '../components/Input';
-import { Envelope, Key } from 'phosphor-react-native';
+import { Input } from "../components/Input";
+import { Envelope, Key } from "phosphor-react-native";
+import { Button } from "../components/Button";
 
 export default function SignIn() {
   const { colors } = useTheme();
   return (
-    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24} >
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Acesse sua conta
@@ -15,14 +16,18 @@ export default function SignIn() {
       <Input
         placeholder="E-mail"
         mb={4}
-        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
       />
       <Input
         placeholder="Senha"
-        mb={4}
+        mb={8}
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        secureTextEntry
       />
 
+      <Button title="Entrar" w="full" />
     </VStack>
   );
 }
