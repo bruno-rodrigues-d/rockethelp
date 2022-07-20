27c02ff1c5ff6cc69c5aab59b74cb7d4657c6c27
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2022-07-20 14:56:43 -0300

    useState name and password

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -3,9 +3,13 @@ import Logo from "../assets/logo_primary.svg";
 import { Input } from "../components/Input";
 import { Envelope, Key } from "phosphor-react-native";
 import { Button } from "../components/Button";
+import { useState } from "react";
 
 export default function SignIn() {
   const { colors } = useTheme();
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
       <Logo />
@@ -19,12 +23,15 @@ export default function SignIn() {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
+        onChangeText={setName}
       />
+
       <Input
         placeholder="Senha"
         mb={8}
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        onChangeText={setPassword}
       />
 
       <Button title="Entrar" w="full" />
